23fa8172b59adc9ac3a1942f48008b8673f5f6fa
AlanS2405 <alan.dario.solis@gmail.com>
2023-07-06 17:15:33 -0300

    fourty commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,15 +6,17 @@ const Hero = () => {
 
   return (
     <section className="hero">
-        <h1 className='hero_greetings'>Hi :) my name is</h1>
+        <h1 className='hero_greetings'>{'< Hi :) my name is >'}</h1>
         <h2 className='hero_name'>Alan Solís.
             <span>And I'm a Front End Developer.</span>
         </h2>
-        <p className='hero_presentation'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis 
-            est cumque iusto libero maxime, nostrum, incidunt, voluptas odit cupiditate 
-            minus omnis aspernatur quod iste id ratione exercitationem veniam dolore? Alias!
+        <p className='hero_presentation'> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Reiciendis est cumque iusto libero maxime, nostrum, incidunt, 
+            voluptas odit cupiditate minus omnis aspernatur quod iste id 
+            ratione exercitationem veniam dolore? Alias!
         </p>
-        <button className='hero_btn'>Contact Me</button>
+        <button className='hero_btn'>{'<'} Contact Me {'>'}</button>
     </section>
   )
 }
