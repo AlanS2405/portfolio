440bf2cf9d6c5ff36ede9eee8ab93ee1c97b0c27
AlanS2405 <alan.dario.solis@gmail.com>
2023-10-16 15:00:00 -0300

    works changed

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -15,10 +15,10 @@ const Works = () => {
             </a>
           </h3>
           <p className="featuredProyect_description">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro est,
-            repellat consectetur labore doloremque minima similique
-            exercitationem velit quam vitae ad dolorum dolore ratione ipsam a
-            nostrum ex quis assumenda.
+            Proyecto de E-commerce full stack que incluye una página de registro
+            y login, filtros de búsqueda, un carrito de compras funcional y una
+            lista que muestra las compras realizadas, garantizando una
+            experiencia fluida y completa para los usuarios.
           </p>
           <ul className="featuredProyect_lenguages_container">
             <li className="featuredProyect_lenguages_items">React</li>
@@ -96,6 +96,14 @@ const Works = () => {
                     <li className="otherProjects_links_items">
                       <a
                         target="_blank"
+                        href="https://github.com/AlanS2405/Users-CRUD-API"
+                      >
+                        <i className="bx bxl-github"></i>
+                      </a>
+                    </li>
+                    <li className="otherProjects_links_items">
+                      <a
+                        target="_blank"
                         href="https://users-crud-alan-solis.netlify.app/"
                       >
                         <i className="bx bx-link-external"></i>
@@ -112,15 +120,81 @@ const Works = () => {
                   </a>
                 </h3>
                 <p className="otherProjects_item_description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  soluta beatae cumque ea voluptas necessitatibus modi quas
-                  commodi ad? Repellendus, eum. Nemo repellat, laudantium natus
-                  laboriosam sapiente repudiandae distinctio corrupti.
+                  Proyecto de Crud de usuarios full stack que permite registrar
+                  y eliminar usuarios, así como editar los datos de cada uno de
+                  ellos una vez hayan sido registrados en el sistema a partir de
+                  un formulario popup.
                 </p>
                 <ul className="otherProjects_lenguages_container">
                   <li className="otherProjects_lenguages">React</li>
                   <li className="otherProjects_lenguages">JavaScript</li>
                   <li className="otherProjects_lenguages">CSS</li>
+                  <li className="otherProjects_lenguages">NodeJs</li>
+                  <li className="otherProjects_lenguages">Express</li>
+                  <li className="otherProjects_lenguages">PostgreSQL</li>
+                </ul>
+              </div>
+            </a>
+          </li>
+          <li className="otherProjects_item">
+            <a
+              target="_blank"
+              className="otherProjects_link"
+              href="https://galleta-de-la-fortuna-alan-solis.netlify.app/"
+            >
+              <div className="otherProjects_container">
+                <ul className="otherProjects_head">
+                  <li className="bx bx-folder_container">
+                    <i className="bx bx-folder"></i>
+                  </li>
+                  <div className="otherProjects_links_container">
+                    <li className="otherProjects_links_items">
+                      <a
+                        target="_blank"
+                        href="https://github.com/AlanS2405/front-auth-and-emailing"
+                      >
+                        <i className="bx bxl-github"></i>
+                      </a>
+                    </li>
+                    <li className="otherProjects_links_items">
+                      <a
+                        target="_blank"
+                        href="https://github.com/AlanS2405/authentication_and_emailing"
+                      >
+                        <i className="bx bxl-github"></i>
+                      </a>
+                    </li>
+                    <li className="otherProjects_links_items">
+                      <a
+                        target="_blank"
+                        href="https://auth-and-emailing-alan-solis.netlify.app/"
+                      >
+                        <i className="bx bx-link-external"></i>
+                      </a>
+                    </li>
+                  </div>
+                </ul>
+                <h3 className="otherProjects_item_title">
+                  <a
+                    target="_blank"
+                    href="https://auth-and-emailing-alan-solis.netlify.app/"
+                  >
+                    Emailing App
+                  </a>
+                </h3>
+                <p className="otherProjects_item_description">
+                  App full Stack de emailing que permite registrar usuarios,
+                  verificar su autenticidad a partir de un email que se envía
+                  con un link de verificación, y también recuperar la contraseña
+                  de este de la misma manera.
+                </p>
+                <ul className="otherProjects_lenguages_container">
+                  <li className="otherProjects_lenguages">React</li>
+                  <li className="otherProjects_lenguages">JavaScript</li>
+                  <li className="otherProjects_lenguages">CSS</li>
+                  <li className="otherProjects_lenguages">NodeJs</li>
+                  <li className="otherProjects_lenguages">Express</li>
+                  <li className="otherProjects_lenguages">PostgreSQL</li>
                 </ul>
               </div>
             </a>
@@ -161,10 +235,10 @@ const Works = () => {
                   </a>
                 </h3>
                 <p className="otherProjects_item_description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  soluta beatae cumque ea voluptas necessitatibus modi quas
-                  commodi ad? Repellendus, eum. Nemo repellat, laudantium natus
-                  laboriosam sapiente repudiandae distinctio corrupti.
+                  Proyecto de PokeApp front-end construído a partir de la api de
+                  pokemons que permite visualizar cada uno de los pokemons
+                  registrados en ella en forma de cartas distribuídas en una
+                  grilla, con filtro de búsqueda y paginación.
                 </p>
                 <ul className="otherProjects_lenguages_container">
                   <li className="otherProjects_lenguages">React</li>
@@ -213,10 +287,10 @@ const Works = () => {
                   </a>
                 </h3>
                 <p className="otherProjects_item_description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  soluta beatae cumque ea voluptas necessitatibus modi quas
-                  commodi ad? Repellendus, eum. Nemo repellat, laudantium natus
-                  laboriosam sapiente repudiandae distinctio corrupti.
+                  Proyecto de app de Rick and Morty front-end construído a
+                  partir de la api de Rick and Morty que permite visualizar cada
+                  uno de los personajes de la serie registrados en ella en forma
+                  de cartas distribuídas en una grilla, con filtro de búsqueda.
                 </p>
                 <ul className="otherProjects_lenguages_container">
                   <li className="otherProjects_lenguages">React</li>
@@ -265,62 +339,10 @@ const Works = () => {
                   </a>
                 </h3>
                 <p className="otherProjects_item_description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  soluta beatae cumque ea voluptas necessitatibus modi quas
-                  commodi ad? Repellendus, eum. Nemo repellat, laudantium natus
-                  laboriosam sapiente repudiandae distinctio corrupti.
-                </p>
-                <ul className="otherProjects_lenguages_container">
-                  <li className="otherProjects_lenguages">React</li>
-                  <li className="otherProjects_lenguages">JavaScript</li>
-                  <li className="otherProjects_lenguages">CSS</li>
-                </ul>
-              </div>
-            </a>
-          </li>
-          <li className="otherProjects_item">
-            <a
-              target="_blank"
-              className="otherProjects_link"
-              href="https://galleta-de-la-fortuna-alan-solis.netlify.app/"
-            >
-              <div className="otherProjects_container">
-                <ul className="otherProjects_head">
-                  <li className="bx bx-folder_container">
-                    <i className="bx bx-folder"></i>
-                  </li>
-                  <div className="otherProjects_links_container">
-                    <li className="otherProjects_links_items">
-                      <a
-                        target="_blank"
-                        href="https://github.com/AlanS2405/galleta-de-la-fortuna"
-                      >
-                        <i className="bx bxl-github"></i>
-                      </a>
-                    </li>
-                    <li className="otherProjects_links_items">
-                      <a
-                        target="_blank"
-                        href="https://galleta-de-la-fortuna-alan-solis.netlify.app/"
-                      >
-                        <i className="bx bx-link-external"></i>
-                      </a>
-                    </li>
-                  </div>
-                </ul>
-                <h3 className="otherProjects_item_title">
-                  <a
-                    target="_blank"
-                    href="https://galleta-de-la-fortuna-alan-solis.netlify.app/"
-                  >
-                    Fortune Cookie APP
-                  </a>
-                </h3>
-                <p className="otherProjects_item_description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  soluta beatae cumque ea voluptas necessitatibus modi quas
-                  commodi ad? Repellendus, eum. Nemo repellat, laudantium natus
-                  laboriosam sapiente repudiandae distinctio corrupti.
+                  App del clima front-end que indica el estado actual del clima
+                  de tu ubicación solicitando permiso desde el buscador para
+                  obtener dicha información del usuario y cambia su estilo en
+                  base al clima que figura actualmente.
                 </p>
                 <ul className="otherProjects_lenguages_container">
                   <li className="otherProjects_lenguages">React</li>
@@ -369,14 +391,14 @@ const Works = () => {
                   </a>
                 </h3>
                 <p className="otherProjects_item_description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-                  soluta beatae cumque ea voluptas necessitatibus modi quas
-                  commodi ad? Repellendus, eum. Nemo repellat, laudantium natus
-                  laboriosam sapiente repudiandae distinctio corrupti.
+                  E-commerce front-end realizado con JavaScript vanilla, HTML y
+                  CSS que consume una api externa para desplegar una grilla de
+                  productos de ropa. Cuenta además con filtro de búsqueda por
+                  categoría, assí como dark mode y pantalla de carga.
                 </p>
                 <ul className="otherProjects_lenguages_container">
-                  <li className="otherProjects_lenguages">React</li>
                   <li className="otherProjects_lenguages">JavaScript</li>
+                  <li className="otherProjects_lenguages">HTML</li>
                   <li className="otherProjects_lenguages">CSS</li>
                 </ul>
               </div>
