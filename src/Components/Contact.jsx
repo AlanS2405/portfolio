966331cb720b5ce20e448032292235091bf74a7e
AlanS2405 <alan.dario.solis@gmail.com>
2023-10-16 15:06:51 -0300

    Contact paragraph changed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,9 +8,10 @@ const Contact = ({ register, handleSubmit, submit }) => {
       <p className="contact_p">
         Currently, I am looking for new job opportunities where I can apply my
         acquired skills and knowledge. If your company is interested in a
-        passionate Front-End Developer with experience in HTML, CSS, JavaScript
-        and React, do not hesitate to contact me. I am available for an
-        interview or any additional information you may need.
+        passionate Full Stack Developer with experience in HTML, CSS,
+        JavaScript, React, NodeJs, Express and PostgreSQL do not hesitate to
+        contact me. I am available for an interview or any additional
+        information you may need.
       </p>
 
       <form className="contact_form" onSubmit={handleSubmit(submit)}>
